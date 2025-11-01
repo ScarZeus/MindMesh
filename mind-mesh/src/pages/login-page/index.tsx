4c79f7bb4b4a4c4dae733b6f7c151f31392ef185
K Kevin Gladson <kevingladson2427@gmail.com>
2025-11-01 16:39:57 +0530

    component setup

---
+++ mind-mesh/src/pages/login-page/index.tsx
@@ -0,0 +1,11 @@
+import style from './index.module.css';
+
+function LoginPage(){
+    return(
+        <div className = {style.homeContainer}>
+            
+        </div>
+    );
+}
+
+export default LoginPage;
